04f44ed950af749946fb74e1fd388c64faf2ca53
moeabdol <mohd.a.saed@gmail.com>
2022-05-02 08:20:33 +0300

    More modifications

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -82,24 +82,24 @@ c.backend = 'webengine'
 # Start flavours
 # base16-qutebrowser (https://github.com/theova/base16-qutebrowser)
 # Base16 qutebrowser template by theova
-# Kanagawa scheme by Originally by rebelot (Ported by montdor [https://github.com/montdor/])
+# Gruvbox dark, hard scheme by Dawid Kurek (dawikur@gmail.com), morhetz (https://github.com/morhetz/gruvbox)
 
-base00 = "#1F1F28"
-base01 = "#2A2A37"
-base02 = "#223249"
-base03 = "#727169"
-base04 = "#C8C093"
-base05 = "#DCD7BA"
-base06 = "#938AA9"
-base07 = "#363646"
-base08 = "#C34043"
-base09 = "#FFA066"
-base0A = "#DCA561"
-base0B = "#98BB6C"
-base0C = "#7FB4CA"
-base0D = "#7E9CD8"
-base0E = "#957FB8"
-base0F = "#D27E99"
+base00 = "#1d2021"
+base01 = "#3c3836"
+base02 = "#504945"
+base03 = "#665c54"
+base04 = "#bdae93"
+base05 = "#d5c4a1"
+base06 = "#ebdbb2"
+base07 = "#fbf1c7"
+base08 = "#fb4934"
+base09 = "#fe8019"
+base0A = "#fabd2f"
+base0B = "#b8bb26"
+base0C = "#8ec07c"
+base0D = "#83a598"
+base0E = "#d3869b"
+base0F = "#d65d0e"
 
 # set qutebrowser colors
 
